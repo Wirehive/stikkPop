cbea8cdaac7e8e0c9f6902f35a1dfeae5c301452
Simon Green <simonjgreen@me.com>
2013-06-28 00:24:47 +0100

    Adjustment to configure window and new build

--- stikkPop/Configure.Designer.cs
+++ stikkPop/Configure.Designer.cs
@@ -49,14 +49,18 @@
             this.autoOpenCheckBox = new System.Windows.Forms.CheckBox();
             this.nameBox = new System.Windows.Forms.TextBox();
             this.endpointBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.tick)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.cross)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 31);
+            this.label1.Location = new System.Drawing.Point(19, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(87, 13);
             this.label1.TabIndex = 1;
@@ -66,7 +70,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(36, 78);
+            this.label2.Location = new System.Drawing.Point(42, 81);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(60, 13);
             this.label2.TabIndex = 2;
@@ -79,7 +83,7 @@
             this.label3.BackColor = System.Drawing.SystemColors.Control;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label3.Location = new System.Drawing.Point(94, 51);
+            this.label3.Location = new System.Drawing.Point(104, 54);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(146, 13);
             this.label3.TabIndex = 4;
@@ -91,7 +95,7 @@
             this.label4.BackColor = System.Drawing.SystemColors.Control;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label4.Location = new System.Drawing.Point(94, 98);
+            this.label4.Location = new System.Drawing.Point(100, 101);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(229, 13);
             this.label4.TabIndex = 5;
@@ -103,7 +107,7 @@
             this.label5.BackColor = System.Drawing.SystemColors.Control;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label5.Location = new System.Drawing.Point(96, 111);
+            this.label5.Location = new System.Drawing.Point(106, 114);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(199, 13);
             this.label5.TabIndex = 6;
@@ -111,7 +115,7 @@
             // 
             // SaveBtn
             // 
-            this.SaveBtn.Location = new System.Drawing.Point(190, 261);
+            this.SaveBtn.Location = new System.Drawing.Point(386, 273);
             this.SaveBtn.Name = "SaveBtn";
             this.SaveBtn.Size = new System.Drawing.Size(75, 23);
             this.SaveBtn.TabIndex = 7;
@@ -122,7 +126,7 @@
             // CancelBtn
             // 
             this.CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.CancelBtn.Location = new System.Drawing.Point(79, 261);
+            this.CancelBtn.Location = new System.Drawing.Point(275, 273);
             this.CancelBtn.Name = "CancelBtn";
             this.CancelBtn.Size = new System.Drawing.Size(75, 23);
             this.CancelBtn.TabIndex = 8;
@@ -133,7 +137,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(19, 139);
+            this.label6.Location = new System.Drawing.Point(25, 142);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(76, 13);
             this.label6.TabIndex = 9;
@@ -142,7 +146,7 @@
             // syntaxBox
             // 
             this.syntaxBox.FormattingEnabled = true;
-            this.syntaxBox.Location = new System.Drawing.Point(99, 136);
+            this.syntaxBox.Location = new System.Drawing.Point(109, 139);
             this.syntaxBox.Name = "syntaxBox";
             this.syntaxBox.Size = new System.Drawing.Size(218, 21);
             this.syntaxBox.TabIndex = 3;
@@ -153,7 +157,7 @@
             this.label7.BackColor = System.Drawing.SystemColors.Control;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label7.Location = new System.Drawing.Point(96, 160);
+            this.label7.Location = new System.Drawing.Point(106, 163);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(178, 13);
             this.label7.TabIndex = 11;
@@ -164,7 +168,7 @@
             this.privateCheckBox.AutoSize = true;
             this.privateCheckBox.Checked = global::stikkPop.Properties.Settings.Default.alwaysPrivate;
             this.privateCheckBox.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::stikkPop.Properties.Settings.Default, "alwaysPrivate", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.privateCheckBox.Location = new System.Drawing.Point(18, 185);
+            this.privateCheckBox.Location = new System.Drawing.Point(28, 188);
             this.privateCheckBox.Name = "privateCheckBox";
             this.privateCheckBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.privateCheckBox.Size = new System.Drawing.Size(95, 17);
@@ -176,7 +180,7 @@
             // tick
             // 
             this.tick.Image = ((System.Drawing.Image)(resources.GetObject("tick.Image")));
-            this.tick.Location = new System.Drawing.Point(323, 28);
+            this.tick.Location = new System.Drawing.Point(333, 31);
             this.tick.Name = "tick";
             this.tick.Size = new System.Drawing.Size(25, 20);
             this.tick.TabIndex = 13;
@@ -187,7 +191,7 @@
             // cross
             // 
             this.cross.Image = ((System.Drawing.Image)(resources.GetObject("cross.Image")));
-            this.cross.Location = new System.Drawing.Point(323, 28);
+            this.cross.Location = new System.Drawing.Point(333, 31);
             this.cross.Name = "cross";
             this.cross.Size = new System.Drawing.Size(25, 20);
             this.cross.TabIndex = 14;
@@ -199,7 +203,7 @@
             this.autoCopyCheckBox.AutoSize = true;
             this.autoCopyCheckBox.Checked = global::stikkPop.Properties.Settings.Default.autoCopy;
             this.autoCopyCheckBox.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::stikkPop.Properties.Settings.Default, "autoCopy", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.autoCopyCheckBox.Location = new System.Drawing.Point(39, 208);
+            this.autoCopyCheckBox.Location = new System.Drawing.Point(235, 222);
             this.autoCopyCheckBox.Name = "autoCopyCheckBox";
             this.autoCopyCheckBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.autoCopyCheckBox.Size = new System.Drawing.Size(74, 17);
@@ -213,7 +217,7 @@
             this.label8.BackColor = System.Drawing.SystemColors.Control;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label8.Location = new System.Drawing.Point(119, 186);
+            this.label8.Location = new System.Drawing.Point(129, 189);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(179, 13);
             this.label8.TabIndex = 16;
@@ -225,7 +229,7 @@
             this.label9.BackColor = System.Drawing.SystemColors.Control;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label9.Location = new System.Drawing.Point(119, 209);
+            this.label9.Location = new System.Drawing.Point(315, 223);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(213, 13);
             this.label9.TabIndex = 17;
@@ -237,7 +241,7 @@
             this.label10.BackColor = System.Drawing.SystemColors.Control;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label10.Location = new System.Drawing.Point(119, 232);
+            this.label10.Location = new System.Drawing.Point(315, 246);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(176, 13);
             this.label10.TabIndex = 19;
@@ -248,7 +252,7 @@
             this.autoOpenCheckBox.AutoSize = true;
             this.autoOpenCheckBox.Checked = global::stikkPop.Properties.Settings.Default.autoOpen;
             this.autoOpenCheckBox.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::stikkPop.Properties.Settings.Default, "autoOpen", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.autoOpenCheckBox.Location = new System.Drawing.Point(38, 231);
+            this.autoOpenCheckBox.Location = new System.Drawing.Point(234, 245);
             this.autoOpenCheckBox.Name = "autoOpenCheckBox";
             this.autoOpenCheckBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.autoOpenCheckBox.Size = new System.Drawing.Size(75, 17);
@@ -259,7 +263,7 @@
             // nameBox
             // 
             this.nameBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::stikkPop.Properties.Settings.Default, "name", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.nameBox.Location = new System.Drawing.Point(97, 75);
+            this.nameBox.Location = new System.Drawing.Point(107, 78);
             this.nameBox.Name = "nameBox";
             this.nameBox.Size = new System.Drawing.Size(220, 20);
             this.nameBox.TabIndex = 2;
@@ -268,11 +272,39 @@
             // endpointBox
             // 
             this.endpointBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::stikkPop.Properties.Settings.Default, "endpoint", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.endpointBox.Location = new System.Drawing.Point(97, 28);
+            this.endpointBox.Location = new System.Drawing.Point(107, 31);
             this.endpointBox.Name = "endpointBox";
             this.endpointBox.Size = new System.Drawing.Size(220, 20);
             this.endpointBox.TabIndex = 1;
             this.endpointBox.Text = global::stikkPop.Properties.Settings.Default.endpoint;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 202);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Text";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Location = new System.Drawing.Point(379, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(350, 202);
+            this.groupBox2.TabIndex = 22;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Images";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(44, 89);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(272, 13);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Currently all image pastes are sent to Imgur anonymously";
             // 
             // Configure
             // 
@@ -280,7 +312,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.CancelBtn;
-            this.ClientSize = new System.Drawing.Size(349, 301);
+            this.ClientSize = new System.Drawing.Size(741, 312);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.autoOpenCheckBox);
             this.Controls.Add(this.label9);
@@ -301,12 +334,15 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.endpointBox);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Configure";
             this.Text = "Configure";
             this.Load += new System.EventHandler(this.Configure_Load);
             ((System.ComponentModel.ISupportInitialize)(this.tick)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.cross)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -334,5 +370,8 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.CheckBox autoOpenCheckBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label11;
     }
 }
